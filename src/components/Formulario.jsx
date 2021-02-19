fba1bf0f13355ea694a6737e94381e5c088cd975
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-02-19 03:23:24 -0300

    agrega hook useCriptomoneda

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components';
+import useCriptomoneda from '../hooks/useCriptomoneda';
 import useMoneda from '../hooks/useMoneda';
+
 
 const Boton = styled.input`
     margin-top: 20px;
@@ -22,22 +25,41 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+    // state listado criptomonedas
+
+    const [listacripto, guardarCriptomonedas] = useState([]);
+
     const MONEDAS = [
         { codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
         { codigo: 'MXN', nombre: 'Peso Mexicano' },
         { codigo: 'EUR', nombre: 'Euro' },
-        { codigo: 'GBP', nombre: 'Libra Esterlina' }
-        { codigo: 'GBP', nombre: 'Libra' }
+        { codigo: 'GBP', nombre: 'Libra Esterlina' },
+        { codigo: 'CLP', nombre: 'Peso Chileno' }
     ];
 
 
     //utilizamos useMoneda (se pueden utilizar nombres distintaos a los retornados en useMoneda)
-    const [ moneda, SelectMonedas, actualizarState] = useMoneda('elige tu moneda');
+    const [ moneda, SelectMonedas, actualizarState] = useMoneda('elige tu moneda', '', MONEDAS);
+
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '', listacripto)
+
+    useEffect (() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
 
     return ( 
         <form>
 
             <SelectMonedas />
+            
+            <SelectCripto />
 
             <Boton 
                  type="submit"
--- src/hooks/useCriptomoneda.jsx
+++ src/hooks/useCriptomoneda.jsx
@@ -21,12 +21,12 @@ const Select = styled.select`
     font-size: 1.2rem;
 `;
 
-const useMoneda = (label, stateInicial, opciones) => {
+const useCriptomoneda = (label, stateInicial, opciones) => {
 
     //state custom hook
     const [state, actualizarState] = useState('');
 
-    const Seleccionar = () => (
+    const SelectCripto = () => (
         <Fragment>
             <Label>Elige tu Moneda</Label>
             <Select
@@ -35,14 +35,14 @@ const useMoneda = (label, stateInicial, opciones) => {
             >
                 <option value="">Seleccione</option>
                 {opciones.map(opcion => (
-                    <option key={opcion.codigo} value={opcion.codigo} >{opcion.nombre}</option>
-                ))}
+                    <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name} >{opcion.CoinInfo.FullName}</option>
+                ))} 
             </Select>
         </Fragment>
     );
 
     //retornar state, interfaz y funcion que modifica el state
-    return [state, Seleccionar, actualizarState];
+    return [state, SelectCripto, actualizarState];
 }
 
-export default useMoneda;
+export default useCriptomoneda;
