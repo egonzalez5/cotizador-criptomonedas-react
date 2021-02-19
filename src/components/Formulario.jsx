11ed3513ba29989915e01240de7a669b1c65c54d
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-02-18 23:15:40 -0300

    formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -27,7 +27,7 @@ const Formulario = () => {
         { codigo: 'MXN', nombre: 'Peso Mexicano' },
         { codigo: 'EUR', nombre: 'Euro' },
         { codigo: 'GBP', nombre: 'Libra Esterlina' }
-        { codigo: 'GBP', nombre: 'Libra Esterlina' }
+        { codigo: 'GBP', nombre: 'Libra' }
     ];
 
 
